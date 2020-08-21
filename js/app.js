143fb98c6375bbe15924ba83de735f5a3ab2d64e
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-21 16:56:42 -0400

    updated the page with a google captcha

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ class Form extends React.Component {
           <input type="text" id="url" onKeyUp={this.props.getUrlInput}></input>
           <label htmlFor="phrase">Enter a word or phrase to scan for</label>
           <input type="text" id="phrase" onKeyUp={this.props.getWordInput}></input>
-          <p>Captchas here</p>
+          <div className="g-recaptcha" data-sitekey="6Ld7-MEZAAAAAA_UuOFPKll6Qs8yWYmzd8d34_0p" data-callback="verifyCaptcha"></div>
+          <div id="g-recaptcha-error"></div>
           <input type="submit" value="Scan Website Now"></input>
         </form>
       </div>
@@ -55,6 +56,14 @@ class App extends React.Component {
 //Search
   search = (event) => {
     event.preventDefault();
+    const response = grecaptcha.getResponse();
+    if(response.length === 0) {
+      document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Please confirm you are not a robot.</span>';
+
+      return false
+    } else {
+      document.getElementById('g-recaptcha-error').innerHTML = '';
+    }
     this.setState(
       {results: `Searching for the word ${this.state.searchWord} at the url ${this.state.searchUrl}`}
     )
